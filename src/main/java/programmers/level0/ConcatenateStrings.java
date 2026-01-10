4f710b091732c1a1d3fe64c641e4046c8cb4a938
yjsiuuuuu <yjs47522@gmail.com>
2026-01-10 17:01:14 +0900

    ConcatenateStrings 설명추가

--- src/main/java/programmers/level0/ConcatenateStrings.java
+++ src/main/java/programmers/level0/ConcatenateStrings.java
@@ -1,5 +1,9 @@
 package programmers.level0;
-
+/*
+문자 리스트를 문자열로 변환하기:
+문자들이 담겨있는 배열 arr가 주어집니다.
+arr의 원소들을 순서대로 이어 붙인 문자열을 return 하는 solution함수를 작성해 주세요.
+*/
 public class ConcatenateStrings {
     public String solution(String[] arr) {
         String answer="";
