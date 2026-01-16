dcce1737e959d1ad3e2cf0add13ffecc10e24dfd
yjsiuuuuu <yjs47522@gmail.com>
2026-01-16 20:07:54 +0900

    ControlNumber1

---
+++ src/main/java/programmers/level0/ControlNumber1.java
@@ -0,0 +1,27 @@
+package programmers.level0;
+
+public class ControlNumber1 {
+    public int solution(int n, String control) {
+
+        for(int i=0;i<control.length();i++)
+        {
+            if(control.charAt(i)=='w')
+            {
+                n+=1;
+            } else if (control.charAt(i)=='s')
+            {
+                n-=1;
+            }else if (control.charAt(i)=='d')
+            {
+                n+=10;
+            }
+            else
+            {
+                n-=10;
+            }
+
+        }
+        return n;
+    }
+}
+
