741b11faba1d8d4ab0fac70c6a008e032829c888
yjsiuuuuu <yjs47522@gmail.com>
2026-01-30 21:11:21 +0900

    RemainderByNine

---
+++ src/main/java/programmers/level0/CountUpperAndLowerLetters.java
@@ -0,0 +1,25 @@
+package programmers.level0;
+/*
+문자 개수 세기:
+알파벳 대소문자로만 이루어진 문자열 my_string이 주어질 때,
+ my_string에서 'A'의 개수, my_string에서 'B'의 개수,...,
+ my_string에서 'Z'의 개수, my_string에서 'a'의 개수, my_string에서 'b'의 개수,...,
+my_string에서 'z'의 개수를 순서대로 담은 길이 52의 정수 배열을 return 하는 solution 함수를 작성해 주세요.
+ */
+public class CountUpperAndLowerLetters {
+    public int[] solution(String my_string) {
+        int[] counts = new int[52];
+
+        for (int i = 0; i < my_string.length(); i++) {
+            char ch = my_string.charAt(i);
+
+            if (ch >= 'A' && ch <= 'Z') {
+                counts[ch - 'A']++;
+            } else { // 'a' ~ 'z'
+                counts[26 + (ch - 'a')]++;
+            }
+        }
+
+        return counts;
+    }
+}
---
+++ src/main/java/programmers/level0/RemainderByNine.java
@@ -0,0 +1,16 @@
+package programmers.level0;
+/*
+9로 나눈 나머지:
+음이 아닌 정수를 9로 나눈 나머지는 그 정수의 각 자리 숫자의 합을 9로 나눈 나머지와 같은 것이 알려져 있습니다.
+이 사실을 이용하여 음이 아닌 정수가 문자열 number로 주어질 때, 이 정수를 9로 나눈 나머지를 return 하는 solution 함수를 작성해주세요.
+*/
+public class RemainderByNine {
+    public int solution(String number) {
+        int sum = 0;
+        for(int i =0 ;i<number.length();i++)
+        {
+            sum = sum + number.charAt(i)-'0';
+        }//charAt()은 숫자가 아니라 문자라서, '0'을 빼줘야 문자 코드가 실제 숫자로 바뀐다.
+        return sum%9;
+    }
+}
