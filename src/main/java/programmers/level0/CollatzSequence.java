97931a73c232f625d640dcbbabcae9483fce1bfb
yjsiuuuuu <yjs47522@gmail.com>
2026-02-03 19:38:55 +0900

    CollatzSequence

---
+++ src/main/java/programmers/level0/CollatzSequence.java
@@ -0,0 +1,26 @@
+package programmers.level0;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CollatzSequence {
+    public int[] solution(int n) {
+        List<Integer> seq = new ArrayList<>();
+        seq.add(n);
+
+        while (n != 1) {
+            if (n % 2 == 0) {
+                n /= 2;
+            } else {
+                n = 3 * n + 1;
+            }
+            seq.add(n);
+        }
+
+        int[] answer = new int[seq.size()];
+        for (int i = 0; i < seq.size(); i++) {
+            answer[i] = seq.get(i);
+        }
+        return answer;
+    }
+}
